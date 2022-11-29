0068f53f508fa993ba68fefa8a2c7fa15c77449c
Kirill Machnev <machnev999@gmail.com>
2022-11-30 01:12:40 +0300

    add working test

--- src/test/java/NewCardOrderTest.java
+++ src/test/java/NewCardOrderTest.java
@@ -1,40 +1,50 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
-
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.chrome.ChromeOptions;
 
-class NewCardOrderTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    WebDriver driver;
+public class NewCardOrderTest {
+
+    private ChromeDriver driver;
 
     @BeforeAll
-    static void setupAll(){
-        WebDriverManager.chromedriver().setup();
+    public static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "./driver/chromedriver.exe");
     }
 
     @BeforeEach
-    void setup(){
+    public void setUp() {
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-dev-shm-usage");
         options.addArguments("--no-sandbox");
         options.addArguments("--headless");
-        driver = new ChromeDriver();
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999/");
     }
 
     @AfterEach
-    void teardown(){
+    public void tearDown() {
         driver.quit();
         driver = null;
     }
 
+    //Заполняем все поля. Заявка оформляется успешно
     @Test
-    void test(){
-      driver.get("http://localhost:9999");
-    }
+    public void SendSuccesForm() {
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Иван Иван");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79998887766");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
 
+        String actualText = driver.findElement(By.cssSelector("[data-test-id='order-success']")).getText().trim();
+        String expectedText = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+
+        assertEquals(expectedText, actualText, "Текст сообщения не совпадает");
+    }
 }
